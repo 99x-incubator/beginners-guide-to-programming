3a5f96903bcee7ea4f81084842687c56a5a88db8
Sachithra Dilshan <itismesachithra@gmail.com>
2020-01-07 02:40:43 +0530

    Created 3 main components named as Dashboard, Gallery, Table of Content and configure routing

---
+++ beginners-guide/src/app/tableofcontent/tableofcontent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tableofcontent',
+  templateUrl: './tableofcontent.component.html',
+  styleUrls: ['./tableofcontent.component.css']
+})
+export class TableofcontentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
